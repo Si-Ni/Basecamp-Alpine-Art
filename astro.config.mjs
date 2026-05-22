089f2f22f63ba69bdf78a0c8b61d021dea4750b9
Simon Nikolaidis <s-nikolaidis@web.de>
2026-05-22 15:43:45 +0200

    feat: add sitemap support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
   output: "server",
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   adapter: cloudflare(),
   site: "https://www.basecamp-alpine-art.de",
 });
